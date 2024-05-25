22be622b57f575842e2e65f90cf28845597391aa
Ziad OUALHADJ <ziadouapro@outlook.com>
2024-05-26 00:55:09 +0200

    Cleaned up the file and added information about what it does

--- settings_utils.py
+++ settings_utils.py
@@ -1,4 +1,19 @@
+"""
+88  88  88           settings_utils.py
+88  ""  88           
+88      88           Collection de fonctions pour la gestion du fichier paramètres
+88  88  88,dPPYba,   
+88  88  88P'    "8a  Dépendants : main.py
+88  88  88       d8  Dépendances : voir "Importations"
+88  88  88b,   ,a8"  
+88  88  8Y"Ybbd8"'   
+"""
+
+# ═══════════════════════════════ IMPORTATIONS ═══════════════════════════════
+
 import shutil
+
+# ═════════════════════════════════ FONCTIONS ════════════════════════════════
 
 def check_settings_file():
     try: # On essaie d'ouvrir le fichier paramètres
